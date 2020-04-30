8f2cb9b6e8f2543ac3236b7fcb2ac2114b751be4
harinarayan14 <gjayashankar2008@gmail.com>
2020-04-30 09:37:01 +0530

    part 1

--- sketch.js
+++ sketch.js
@@ -1,31 +1,141 @@
-var canvas, backgroundImage;
-
-var gameState = 0;
-var playerCount;
-var allPlayers;
-var distance = 0;
 var database;
 
-var form, player, game;
-
-var cars, car1, car2, car3, car4;
+var brush;
+var pos = [];
+var position= [];
+var col = "black";
 
 
 function setup(){
-  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  createCanvas(1200, 600);
+  //database = firebase.database();
+  line(1000,0,1000,600);
+  red1 = createSprite(1040,50,40,40);
+  red1.shapeColor = "red";
+  button1 = createButton("click");
+  button1.position(1020,50);
+  blue1 = createSprite(1100,50,40,40);
+  blue1.shapeColor = "blue";
+  button2 = createButton("click");
+  button2.position(1080,50);
+  green1 = createSprite(1160,50,40,40);
+  green1.shapeColor = "green";
+  button3 = createButton("click");
+  button3.position(1140,50);
+  yellow1 = createSprite(1040,150,40,40);
+  yellow1.shapeColor = "yellow";
+  button4 = createButton("click");
+  button4.position(1020,150);
+  black1 = createSprite(1100,150,40,40);
+  black1.shapeColor = "black";
+  button5 = createButton("click");
+  button5.position(1080,150);
+  white1 = createSprite(1160,150,40,40);
+  white1.shapeColor = "white";
+  button6 = createButton("erase");
+  button6.position(1140,150);
+  lime1 = createSprite(1040,250,40,40);
+  lime1.shapeColor = "lime";
+  button7 = createButton("click");
+  button7.position(1020,250);
+  pink1 = createSprite(1100,250,40,40);
+  pink1.shapeColor = "pink";
+  button8 = createButton("click");
+  button8.position(1080,250);
+  aqua1 = createSprite(1160,250,40,40);
+  aqua1.shapeColor = "aqua";
+  button9 = createButton("click");
+  button9.position(1140,250);
+  brown1 = createSprite(1040,350,40,40);
+  brown1.shapeColor = "brown";
+  button10 = createButton("click");
+  button10.position(1020,350);
+  purple1 = createSprite(1100,350,40,40);
+  purple1.shapeColor = "purple";
+  button11 = createButton("click");
+  button11.position(1080,350);
+  violet1 = createSprite(1160,350,40,40);
+  violet1.shapeColor = "violet";
+  button12 = createButton("click");
+  button12.position(1140,350);
+  line(1000,400,1200,400);
+  line(1140,170,1180,170);
+  line(1140,170,1140,130);
+  line(1180,130,1180,170);
+  line(1140,130,1180,130);
+  Slider1 = createSlider(0,100,0);
+  Slider1.position(1020,500);
   database = firebase.database();
-  game = new Game();
-  game.getState();
-  game.start();
+  var ballPosition = database.ref("position/pos");
+  ballPosition.on("value",readOperator,showError);
+  colour();
 }
 
 
-function draw(){
-  if(playerCount === 4){
-    game.update(1);
-  }
-  if(gameState === 1){
-    clear();
-    game.play();
-  }
+function draw() {
+  drawSprites();
+}
+function mouseDragged(){
+  database.ref("position").set({
+pos : pos
+  })
+pos=[mouseX,mouseY];
+position.push(pos);
+  var fVal = Slider1.value();
+  w =map(fVal, 0, 100, 1, 5);
+  for(var i = 0;i<position.length;i++){
+    noStroke();
+    fill(col);
+   rect(position[i][0],position[i][1],w,w);
+}
+}
+function readOperator(data){
+  var positio = data.val();
+  console.log(positio);
+  for(var i = 0;i<position.length;i++){
+  position[i][0] = pos.x;
+  position[i][1] = pos.y;
+}
+}
+
+function showError(){
+  console.log("error");
+}
+function colour(){
+  button1.mousePressed(function(){
+    col = "red";
+  })
+  button2.mousePressed(function(){
+    col = "blue";
+  })
+  button3.mousePressed(function(){
+    col = "green";
+  })
+  button4.mousePressed(function(){
+    col = "yellow";
+  })
+  button5.mousePressed(function(){
+    col = "black";
+  })
+  button6.mousePressed(function(){
+    col = "white";
+  })
+  button7.mousePressed(function(){
+    col = "lime";
+  })
+  button8.mousePressed(function(){
+    col = "pink";
+  })
+  button9.mousePressed(function(){
+    col = "aqua";
+  })
+  button10.mousePressed(function(){
+    col = "brown";
+  })
+  button11.mousePressed(function(){
+    col = "purple";
+  })
+  button12.mousePressed(function(){
+    col = "violet";
+  })
 }
